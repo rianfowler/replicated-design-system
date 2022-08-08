d3a103dbeb5d7153e3443a81b04d02485496bba4
miaawong <miawongdev@gmail.com>
2022-08-08 13:29:47 -0700

    first

---
+++ src/shared/icons.js
@@ -0,0 +1,167 @@
+import adoptionRate from "./icons/adoption_rate.svg";
+import backArrowBlue from "./icons/back_arrow_blue.svg";
+import backArrowHover from "./icons/back_arrow_hover.svg";
+import backArrow from "./icons/back_arrow.svg";
+import blueRoundedLink from "./icons/blue_rounded_link.svg";
+import closeHover from "./icons/close_hover.svg";
+import close from "./icons/close.svg";
+import codeHover from "./icons/code_hover.svg";
+import codeSlash from "./icons/code_slash.svg";
+import code from "./icons/code.svg";
+import commands from "./icons/commands.svg";
+import communication from "./icons/communication.svg";
+import defaultImageIcon from "./icons/default_image_icon.svg";
+import docker from "./icons/docker.svg";
+import dollarSign from "./icons/dollar_sign.svg";
+import downArrow from "./icons/down_arrow.svg";
+import downArrowHover from "./icons/down_arrow_hover.svg";
+import downloadBlueHover from "./icons/download_blue_hover.svg";
+import downloadBlue from "./icons/download_blue.svg";
+import downloadGrayHover from "./icons/download_gray_hover.svg";
+import downloadGray from "./icons/download_gray.svg";
+import editHover from "./icons/edit_hover.svg";
+import edit from "./icons/edit.svg";
+import errorCircle from "./icons/error_circle.svg";
+import fileDownloadHover from "./icons/file_download_hover.svg";
+import fileDownload from "./icons/file_download.svg";
+import gearHover from "./icons/gear_hover.svg";
+import gear from "./icons/gear.svg";
+import github from "./icons/github.svg";
+import grayClock from "./icons/gray_clock.svg";
+import greenCheck from "./icons/green_check.svg";
+import gridGray from "./icons/grid_gray.svg";
+import gridGrayHover from "./icons/grid_gray_hover.svg";
+import gridWhite from "./icons/grid_white.svg";
+import help from "./icons/help.svg";
+import infoHover from "./icons/info_hover.svg";
+import info from "./icons/info.svg";
+import key from "./icons/key.svg";
+import kubernetes from "./icons/kubernetes.svg";
+import lastBuild from "./icons/last_build.svg";
+import licenses from "./icons/licenses.svg";
+import linksBlueHover from "./icons/links_blue_hover.svg";
+import linksGrayHover from "./icons/links_gray_hover.svg";
+import linksBlue from "./icons/links_blue.svg";
+import linksGray from "./icons/links_gray.svg";
+import listGray from "./icons/list_gray.svg";
+import listGrayHover from "./icons/list_hover.svg";
+import listWhite from "./icons/list_white.svg";
+import lock from "./icons/lock.svg";
+import magnifyingGlassSm from "./icons/magnifying_glass_small.svg";
+import magnifyingGlass from "./icons/magnifying_glass.svg";
+import menuHover from "./icons/menu_hover.svg";
+import menu from "./icons/menu.svg";
+import news from "./icons/news.svg";
+import oval from "./icons/oval.svg";
+import previewHover from "./icons/preview_hover.svg";
+import preview from "./icons/preview.svg";
+import receiptHover from "./icons/receipt_hover.svg";
+import receipt from "./icons/receipt.svg";
+import replicated from "./icons/replicated.svg";
+import reportingHover from "./icons/reporting_hover.svg";
+import reporting from "./icons/reporting.svg";
+import rightArrow from "./icons/right_arrow.svg";
+import selectAllCheck from "./icons/select_all_check.svg";
+import sidebarMenuRightHover from "./icons/sidebar_menu_right_hover.svg";
+import sidebarMenuLeftHover from "./icons/sidebar_menu_left_hover.svg";
+import sidebarMenuLeft from "./icons/sidebar_menu_left.svg";
+import sidebarMenuRight from "./icons/sidebar_menu_right.svg";
+import silverSandTrash from "./icons/silver_sand_trash.svg";
+import slack from "./icons/slack.svg";
+import trashHover from "./icons/trash_hover.svg";
+import trash from "./icons/trash.svg";
+import unarchiveHover from "./icons/unarchive_hover.svg";
+import unarchive from "./icons/unarchive.svg";
+import upArrow from "./icons/up_arrow.svg";
+import uploadBlue from "./icons/upload_blue.svg";
+import user from "./icons/user.svg";
+import warningHover from "./icons/warning_hover.svg";
+import warningYellow from "./icons/warning_yellow.svg";
+import warning from "./icons/warning.svg";
+import CheckWhite from "./icons/check_white.svg";
+import TrashWhite from "./icons/trash_white.svg";
+
+export const icons = {
+  adoptionRate,
+  backArrowBlue,
+  backArrowHover,
+  backArrow,
+  blueRoundedLink,
+  closeHover,
+  close,
+  codeHover,
+  codeSlash,
+  code,
+  commands,
+  communication,
+  defaultImageIcon,
+  docker,
+  dollarSign,
+  downArrow,
+  downArrowHover,
+  downloadBlueHover,
+  downloadBlue,
+  downloadGrayHover,
+  downloadGray,
+  editHover,
+  edit,
+  errorCircle,
+  fileDownloadHover,
+  fileDownload,
+  gearHover,
+  gear,
+  github,
+  grayClock,
+  greenCheck,
+  gridGray,
+  gridGrayHover,
+  gridWhite,
+  help,
+  infoHover,
+  info,
+  key,
+  kubernetes,
+  lastBuild,
+  licenses,
+  lock,
+  linksBlueHover,
+  linksGrayHover,
+  linksBlue,
+  linksGray,
+  listGray,
+  listGrayHover,
+  listWhite,
+  magnifyingGlassSm,
+  magnifyingGlass,
+  menuHover,
+  menu,
+  news,
+  oval,
+  previewHover,
+  preview,
+  receiptHover,
+  receipt,
+  replicated,
+  reportingHover,
+  reporting,
+  rightArrow,
+  selectAllCheck,
+  sidebarMenuRightHover,
+  sidebarMenuLeftHover,
+  sidebarMenuLeft,
+  sidebarMenuRight,
+  silverSandTrash,
+  slack,
+  trashHover,
+  trash,
+  unarchiveHover,
+  unarchive,
+  upArrow,
+  uploadBlue,
+  user,
+  warningHover,
+  warningYellow,
+  warning,
+  CheckWhite,
+  TrashWhite,
+};
